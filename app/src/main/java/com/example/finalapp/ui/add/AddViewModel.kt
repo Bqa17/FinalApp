9ba296c2a62993822811dd909f9f4708e2b18da5
Bqa17 <bq3@njit.edu>
2024-11-28 23:47:37 -0500

    Addapted template to the project

--- app/src/main/java/com/example/finalapp/ui/add/AddViewModel.kt
+++ app/src/main/java/com/example/finalapp/ui/add/AddViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.finalapp.ui.notifications
+package com.example.finalapp.ui.add
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class AddViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
